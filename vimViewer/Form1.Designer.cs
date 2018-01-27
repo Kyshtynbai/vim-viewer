c2ddc939b4ce540cc047d36bb538fcd1ce9de914
Ivan Martynow <imartynow@gmail.com>
2018-01-27 23:35:05 +0300

    Added ToggleFullScreen

--- vimViewer/Form1.Designer.cs
+++ vimViewer/Form1.Designer.cs
@@ -28,65 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.openFileButton = new System.Windows.Forms.Button();
-            this.opernDirButton = new System.Windows.Forms.Button();
-            this.nextImageButton = new System.Windows.Forms.Button();
-            this.previousImageButton = new System.Windows.Forms.Button();
-            this.closeButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.label2 = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.fullScreenButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.nextImageButton = new System.Windows.Forms.Button();
+            this.previousImageButton = new System.Windows.Forms.Button();
+            this.opernDirButton = new System.Windows.Forms.Button();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.testButton = new System.Windows.Forms.Button();
             this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // openFileDialog1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.257918F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 91.74208F));
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 3);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89.28571F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.71429F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(884, 417);
-            this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // pictureBox1
-            // 
-            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 2);
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(878, 327);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(3, 336);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(39, 17);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "full";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // flowLayoutPanel1
             // 
@@ -95,31 +57,33 @@
             this.flowLayoutPanel1.Controls.Add(this.previousImageButton);
             this.flowLayoutPanel1.Controls.Add(this.nextImageButton);
             this.flowLayoutPanel1.Controls.Add(this.closeButton);
+            this.flowLayoutPanel1.Controls.Add(this.fullScreenButton);
+            this.flowLayoutPanel1.Controls.Add(this.testButton);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(75, 336);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(75, 375);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(806, 33);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(806, 39);
             this.flowLayoutPanel1.TabIndex = 2;
             // 
-            // openFileButton
+            // fullScreenButton
             // 
-            this.openFileButton.Location = new System.Drawing.Point(3, 3);
-            this.openFileButton.Name = "openFileButton";
-            this.openFileButton.Size = new System.Drawing.Size(75, 23);
-            this.openFileButton.TabIndex = 0;
-            this.openFileButton.Text = "file";
-            this.openFileButton.UseVisualStyleBackColor = true;
-            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            this.fullScreenButton.Location = new System.Drawing.Point(408, 3);
+            this.fullScreenButton.Name = "fullScreenButton";
+            this.fullScreenButton.Size = new System.Drawing.Size(75, 23);
+            this.fullScreenButton.TabIndex = 5;
+            this.fullScreenButton.Text = "fullscreen";
+            this.fullScreenButton.UseVisualStyleBackColor = true;
+            this.fullScreenButton.Click += new System.EventHandler(this.fullScreenButton_Click);
             // 
-            // opernDirButton
+            // closeButton
             // 
-            this.opernDirButton.Location = new System.Drawing.Point(84, 3);
-            this.opernDirButton.Name = "opernDirButton";
-            this.opernDirButton.Size = new System.Drawing.Size(75, 23);
-            this.opernDirButton.TabIndex = 1;
-            this.opernDirButton.Text = "dir";
-            this.opernDirButton.UseVisualStyleBackColor = true;
-            this.opernDirButton.Click += new System.EventHandler(this.opernDirButton_Click);
+            this.closeButton.Location = new System.Drawing.Point(327, 3);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 4;
+            this.closeButton.Text = "close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
             // 
             // nextImageButton
             // 
@@ -141,39 +105,73 @@
             this.previousImageButton.UseVisualStyleBackColor = true;
             this.previousImageButton.Click += new System.EventHandler(this.previousImageButton_Click);
             // 
-            // closeButton
+            // opernDirButton
             // 
-            this.closeButton.Location = new System.Drawing.Point(327, 3);
-            this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(75, 23);
-            this.closeButton.TabIndex = 4;
-            this.closeButton.Text = "close";
-            this.closeButton.UseVisualStyleBackColor = true;
-            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            this.opernDirButton.Location = new System.Drawing.Point(84, 3);
+            this.opernDirButton.Name = "opernDirButton";
+            this.opernDirButton.Size = new System.Drawing.Size(75, 23);
+            this.opernDirButton.TabIndex = 1;
+            this.opernDirButton.Text = "dir";
+            this.opernDirButton.UseVisualStyleBackColor = true;
+            this.opernDirButton.Click += new System.EventHandler(this.opernDirButton_Click);
             // 
-            // label1
+            // openFileButton
             // 
-            this.label1.AutoSize = true;
-            this.tableLayoutPanel1.SetColumnSpan(this.label1, 2);
-            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Location = new System.Drawing.Point(3, 372);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(878, 24);
-            this.label1.TabIndex = 3;
+            this.openFileButton.Location = new System.Drawing.Point(3, 3);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(75, 23);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "file";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
             // 
-            // openFileDialog1
+            // checkBox1
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 375);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(39, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "full";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // pictureBox1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label2.Location = new System.Drawing.Point(3, 396);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 21);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Counter: ";
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 2);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(878, 366);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.257918F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 91.74208F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.71429F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(884, 417);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(489, 3);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(75, 23);
+            this.testButton.TabIndex = 6;
+            this.testButton.Text = "TEST ROWS";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
             // 
             // Form1
             // 
@@ -181,32 +179,33 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 417);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.KeyPreview = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "vimViewer";
             this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button openFileButton;
         private System.Windows.Forms.Button opernDirButton;
-        private System.Windows.Forms.Button nextImageButton;
         private System.Windows.Forms.Button previousImageButton;
+        private System.Windows.Forms.Button nextImageButton;
         private System.Windows.Forms.Button closeButton;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button fullScreenButton;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button testButton;
     }
 }
 
--- vimViewer/Form1.cs
+++ vimViewer/Form1.cs
@@ -13,25 +13,41 @@ namespace vimViewer
 {
     public partial class Form1 : Form
     {
-        string[] currentDirFiles; // array of files of a chosen directory
-        string currentDir = @"e:\tmp\tmp\"; // default directory
-        int counter; // current images's index currentDirFiles[0 1 2 |3| 4 5 6] for example
-        int filesInDir; // array length (i.e. files in a directory)
+        string[] currentDirFiles;
+        string currentDir = @"e:\tmp\tmp\";
+        int counter;
+        int filesInDir;
+        bool isFullScreen = false;
 
         public Form1()
         {
             InitializeComponent();
+            this.KeyPreview = true;
             var t = getFilesCount(currentDir);
             currentDirFiles = t.Item2;
             filesInDir = t.Item1;
             counter = 0;
-            //showImage(currentDirFiles[0], counter);
+            showImage(currentDirFiles[0], counter);
         }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Q))
+            {
+                this.Close();
+                // return true;
+            }
+            if (keyData == (Keys.F))
+            {
+                toggleFullScreen(isFullScreen);
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
         private void showImage(string fileName, int counter)
         {
             pictureBox1.Load(fileName);
-            label1.Text = fileName;
-            label2.Text = $"{counter}";
         }
 
         private Tuple<int, string[]> getFilesCount(string dirName)
@@ -45,7 +61,6 @@ namespace vimViewer
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                label1.Text = openFileDialog1.FileName;
                 pictureBox1.Load(openFileDialog1.FileName);
 
             }
@@ -55,7 +70,6 @@ namespace vimViewer
         {
             if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
-                label1.Text = folderBrowserDialog1.SelectedPath;
                 currentDir = folderBrowserDialog1.SelectedPath;
                 var t = getFilesCount(currentDir);
                 currentDirFiles = t.Item2;
@@ -82,6 +96,36 @@ namespace vimViewer
         private void closeButton_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void fullScreenButton_Click(object sender, EventArgs e)
+        {
+            toggleFullScreen(isFullScreen);
+        }
+
+        private void testButton_Click(object sender, EventArgs e)
+        {
+            toggleFullScreen(isFullScreen);
+        }
+
+        private void toggleFullScreen(bool isFullScreen)
+        {
+            if (!isFullScreen)
+            {
+                this.FormBorderStyle = FormBorderStyle.None;
+                tableLayoutPanel1.RowStyles[1].SizeType = SizeType.Absolute;
+                tableLayoutPanel1.RowStyles[1].Height = 0;
+                this.isFullScreen = true;
+
+            }
+            else
+            {
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                this.WindowState = FormWindowState.Maximized;
+                tableLayoutPanel1.RowStyles[1].SizeType = SizeType.Percent;
+                tableLayoutPanel1.RowStyles[1].Height = 5;
+                this.isFullScreen = false;
+            }
         }
     }
 }
